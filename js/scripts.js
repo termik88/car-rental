237f31275d22b10ffa461c25270183073ccebfdb
termik88 <dirkser@mail.ru>
2017-10-08 23:41:22 +0500

    Окончание верстки сетки

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: new google.maps.LatLng(59.93926, 30.32915),
+    mapTypeId: 'roadmap'
+  });
+
+  var iconBase = 'img/';
+  var icons = {
+    gllacy: {
+      icon: iconBase + 'pin.png'
+    }
+  };
+
+  var features = [
+    {
+      position: new google.maps.LatLng(59.93866, 30.3229),
+      type: 'gllacy'
+    }
+  ];
+
+  // Create markers.
+  features.forEach(function(feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      icon: icons[feature.type].icon,
+      map: map
+    });
+  });
+}
