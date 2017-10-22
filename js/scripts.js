cc5fd181df554afc0a3f8d4522961cd47d86afe8
termik88 <dirkser@mail.ru>
2017-10-22 10:37:45 +0500

    успешная автоматизация, кнопка в хедер + жс

--- js/scripts.js
+++ js/scripts.js
@@ -31,16 +31,10 @@ function initMap() {
 }
 
 /*Модальное окно выбор размера*/
-var link = document.querySelector(".button-order");
-
-var popup = document.querySelector(".modal");
-var wrapper = document.querySelector(".modal-wrapper");
-
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-  wrapper.classList.add("modal-wrapper-show")
-});
+var linkIndex = document.querySelector('.hit__button');
+var popup = document.querySelector('.modal');
+var wrapper = document.querySelector('.modal-wrapper');
+var linkCatalog = document.querySelectorAll('.order__modal-link');
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
@@ -55,3 +49,36 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
+if(linkCatalog != null) {
+  [].forEach.call(linkCatalog, function(item) {
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+      popup.classList.add('modal-show');
+      wrapper.classList.add('modal-wrapper-show');
+    });
+  });
+};
+
+if(linkIndex != null) {
+  linkIndex.addEventListener('click', function(event) {
+    event.preventDefault();
+    popup.classList.add('modal-show');
+    wrapper.classList.add('modal-wrapper-show');
+  });
+};
+
+/*Кнопка в хедаре*/
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
