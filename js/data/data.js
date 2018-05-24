ff0e62fe5991f708bf141d7ae600b7ec7bd2229b
termik88 <dirkser@mail.ru>
2018-05-24 17:54:30 +0500

    second car

---
+++ js/data/data.js
@@ -0,0 +1,18 @@
+const languages = {
+  rus: {
+    promoTitle: `Аренда автомобиля<br>представительского класса<br>с водителем`,
+    category: [`Минивены`, `Автобусы`],
+    cars: [
+      {
+        name: `Mercedes-Benz Viano`,
+        description: `Охуительный тазик, Да, Женя ?)))`,
+        color: `Черный`,
+        places: `6`,
+      }
+
+    ]
+  },
+  eng: {
+    promoTitle: `Rent a car<br>with driver`,
+  }
+};
